357dfc39cd6c86d055ecf3560b56cd3315825a47
Dmitry Nikolaev <dnikolaev@fastmail.com>
2025-05-08 19:35:42 +0100

    Fix testing logic

--- dry_run_lambdag.py
+++ dry_run_lambdag.py
@@ -24,22 +24,41 @@ top_authors = df.author.value_counts()[:10].index.tolist()
 author_to_fragments = {}
 for author in top_authors:
     author_to_fragments[author] = df.loc[df.author == author].text.tolist()
+
+# Predefine train and test splits for all authors
+author_train_test_splits = {}
+for author in author_to_fragments:
+    fragments = author_to_fragments[author][:]
+    random.shuffle(fragments)
+    test_set_size = len(fragments) // 2
+    author_train_test_splits[author] = {
+        "train": fragments[test_set_size:],
+        "test": fragments[:test_set_size],
+    }
+
 print("Computing LambdaG scores...")
 results = defaultdict(dict)
-for author in tqdm(author_to_fragments):
-    target_author_fragments = author_to_fragments[author][:]
-    random.shuffle(target_author_fragments)
-    test_set_size = len(target_author_fragments) // 2
-    train_fragments = target_author_fragments[test_set_size:]
-    test_fragments = target_author_fragments[:test_set_size]
+
+# Train and test for each author
+for author in tqdm(author_train_test_splits):
+    # Get train and test fragments for the current author
+    train_fragments = author_train_test_splits[author]["train"]
+    test_fragments = author_train_test_splits[author]["test"]
+
+    # Train the model on the training set of the current author
     lambda_g.train_known_author_model(train_fragments)
+
+    # Compute LambdaG scores for the test fragments of the current author
     results[author][author] = lambda_g.compute_lambda_g(test_fragments)
-    for other_author in tqdm(author_to_fragments, leave=False):
+
+    # Compute LambdaG scores for the test fragments of other authors
+    for other_author in tqdm(author_train_test_splits, leave=False):
         if other_author == author:
             continue
-        print(f"Computing LambdaG for {author} vs {other_author}...")
-        other_test_fragments = author_to_fragments[other_author]
+        other_test_fragments = author_train_test_splits[other_author]["test"]
         results[author][other_author] = lambda_g.compute_lambda_g(other_test_fragments)
+
+# Write results to file
 print("Writing results to file...")
 with open("lambda_g_results.txt", "w") as f:
     for author, other_authors in results.items():
